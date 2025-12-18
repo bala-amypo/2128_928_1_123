fa1f361c19b9a6b657a9d6b8bf3a25e1f9ae6a04
bala-amypo <bala@amypo.in>
2025-12-18 14:59:10 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/SkillImpl.java
+++ src/main/java/com/example/demo/service/Impl/SkillImpl.java
@@ -29,7 +29,7 @@ public class SkillImpl implements SkillService {
     @Override
     public List<Skill> getAllSkills() {
         return skillRepo.findAll();
-    }
+    } 
 
     @Override
     public Skill updateSkill(Long id, Skill skill) {
