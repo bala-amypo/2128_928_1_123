0a4e4e73233343d5c132b776b30f7b57c4380c97
bala-amypo <bala@amypo.in>
2025-12-25 10:43:42 +0000

    User commit

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -8,17 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://9142.408procr.amypo.ai")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
+@Bean
+public WebMvcConfigurer corsConfigurer() {
+return new WebMvcConfigurer() {
+@Override
+public void addCorsMappings(CorsRegistry registry) {
+registry.addMapping("/**")
+.allowedOrigins("http://9142.408procr.amypo.ai")
+.allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+.allowedHeaders("*")
+.allowCredentials(true);
+}
+};
+}
 }
