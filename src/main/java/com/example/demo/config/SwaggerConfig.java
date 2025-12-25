c092746a1c4061349ad1c98f0cd3eeffd8544e45
bala-amypo <bala@amypo.in>
2025-12-25 07:06:54 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,37 +1,19 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI employeeSkillMatrixOpenAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
-                .servers(List.of(
-                        new Server().url("https://9037.pro604cr.amypo.ai/")
-                ));
-        }
+                .info(new Info()
+                        .title("Employee Skills Matrix Search API")
+                        .description("REST APIs for managing employees, skills, and skill-based search")
+                        .version("1.0"));
+    }
 }
-
-// package com.example.demo.config;
-
-// import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-// import io.swagger.v3.oas.annotations.info.Info;
-// import org.springframework.context.annotation.Configuration;
-
-// @Configuration
-// @OpenAPIDefinition(
-// info = @Info(
-// title = "Employee Matrix API",
-// version = "1.0",
-// description = "API documentation for Employee Skill Matrix Project"
-// )
-// )
-// public class SwaggerConfig {
-// }
