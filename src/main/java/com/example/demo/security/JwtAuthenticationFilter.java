deb7a320f111dd96c31c2e14b226387537061551
bala-amypo <bala@amypo.in>
2025-12-26 16:53:00 +0000

    User commit

---
+++ src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
@@ -0,0 +1,23 @@
+package com.example.demo.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain)
+            throws ServletException, IOException {
+
+        // Do nothing – allow request to pass
+        filterChain.doFilter(request, response);
+    }
+}
