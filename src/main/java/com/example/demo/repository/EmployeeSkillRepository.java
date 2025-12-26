3c49d7a33c9893b616613ac122e04ca9d7f33276
bala-amypo <bala@amypo.in>
2025-12-26 05:01:56 +0000

    User commit

--- src/main/java/com/example/demo/repository/EmployeeSkillRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeSkillRepository.java
@@ -1,27 +1,13 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.Employee;
 import com.example.demo.entity.EmployeeSkill;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface EmployeeSkillRepository extends JpaRepository<EmployeeSkill, Long> {
 
-    List<EmployeeSkill> findByEmployeeIdAndActiveTrue(Long employeeId);
+    List<EmployeeSkill> findByEmployeeId(Long employeeId);
 
-    List<EmployeeSkill> findBySkillIdAndActiveTrue(Long skillId);
-
-    @Query("""
-        SELECT es.employee
-        FROM EmployeeSkill es
-        WHERE es.skill.name IN :skills
-          AND es.employee.active = true
-          AND es.skill.active = true
-        GROUP BY es.employee
-        HAVING COUNT(DISTINCT es.skill.name) = :#{#skills.size()}
-    """)
-    List<Employee> findEmployeesByAllSkillNames(@Param("skills") List<String> skills);
+    List<EmployeeSkill> findBySkillId(Long skillId);
 }
