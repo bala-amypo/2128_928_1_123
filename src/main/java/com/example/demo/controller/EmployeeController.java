7ca3620123980c87f0d07bd88e2e53205d82e41d
bala-amypo <bala@amypo.in>
2025-12-27 02:43:58 +0000

    User commit

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/api/employees")
+@RequestMapping("/employees")
 public class EmployeeController {
 
     @Operation(
